4d187519af5cb591a81f2ec7543447dff6403e38
ncesari49 <cesarin@hotmail.fr>
2023-02-01 14:06:21 +0100

    firstcommit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,36 +1,15 @@
-import Counter from "../components/Counter";
-import logo from "../assets/logo.svg";
+import imgDesc from "@assets/img/imgDesc.jpg";
+import "@assets/styles/Home.css";
 
 export default function Home() {
   return (
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>Hello Vite + React !</p>
-
-      <Counter />
-
-      <p>
-        Edit <code>App.jsx</code> and save to test HMR updates.
-      </p>
-      <p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        {" | "}
-        <a
-          className="App-link"
-          href="https://vitejs.dev/guide/features.html"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Vite Docs
-        </a>
-      </p>
-    </header>
+    <div className="Home">
+      <div className="imgDescription">
+        <img
+          src={imgDesc}
+          alt="paysage de bambou avec un khern au premier plan"
+        />
+      </div>
+    </div>
   );
 }
